646d5ab0ca48be757b7c94736e3b806ec00d058c
Amna Saeed <amnasaeed14@gmail.com>
2026-06-05 17:27:12 +0200

    Remove Evidence.dev BI layer (moving to Power BI)

--- reports/sources/nyc311/backlog.sql
+++
@@ -1,9 +0,0 @@
-select
-    borough,
-    agency_code,
-    total_requests,
-    closed_requests,
-    open_requests,
-    open_overdue_requests,
-    oldest_open_request_age_days
-from NYC311.NYC_311_GOLD.mart_open_vs_closed_status
